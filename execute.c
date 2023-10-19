e9e27cbd442dd2cb924c253c5645a58069a02dc9
Nomee07 <gigabanomfundo@gmail.com>
2023-10-19 00:08:41 -0700

    Monty program

--- execute.c
+++ execute.c
@@ -24,22 +24,31 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	char *op;
 
 	op = strtok(content, " \n\t");
-	if (op && op[0] == '#')
+	if (op == NULL || op[0] == '#')
 		return (0);
+
 	bus.arg = strtok(NULL, " \n\t");
-	while (opst[i].opcode && op)
+
+	if (bus.arg == NULL && strcmp(op, "push") == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	while (opst[i].opcode)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{	opst[i].f(stack, counter);
+		{
+			opst[i].f(stack, counter);
 			return (0);
 		}
 		i++;
 	}
-	if (op && opst[i].opcode == NULL)
-	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-		fclose(file);
-		free(content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE); }
-	return (1);
+	fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+	fclose(file);
+	free(content);
+	free_stack(*stack);
+	exit(EXIT_FAILURE);
 }
